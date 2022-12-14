1120d492d2882d88d31a99b22e2390dc4619eece
Виктор Щеглов <tgeodude@MacBook-Air-Viktor.local>
2022-12-14 17:39:35 +0300

    laba1

--- sem_5/Kostin/ex01/inter.cpp
+++ sem_5/Kostin/ex01/inter.cpp
@@ -27,20 +27,37 @@ int main()
         cout << " " << arr[i][0] << " " << arr[i][1] << endl;
 
     float x1, x3, y1, y3;
+    float arr1[count][2];
 
-    x1 = arr[0][0];
-    x3 = arr[count - 1][0];
-    y1 = arr[0][1];
-    y3 = arr[count - 1][1];
     cout << "Точки которые остались после сжатия методом интерполяции" << endl;
+    int k = 0;
     for (int i = 0; i < count; i++) {
         float x2, y2, tmp;
-        x2 = arr[i][0];
-        tmp = arr[i][1];
-        y2 = (((x2 - x1) * (y3 - y1))/(x3 - x1)) + y1;
-        if (fabs(y2 - tmp) <= e)
+        x1 = arr[i][0];
+        y1 = arr[i][1];
+        if (i + 2 >= count)
+            break;
+        x3 = arr[i + 2][0];
+        y3 = arr[i + 2][1];
+        x2 = arr[i + 1][0];
+        y2 = arr[i + 1][1];
+        tmp = fabs(y2 - (y1 + (((fabs(y3 - y1)) / 2))));
+        if (tmp <= e)
+        {
+            arr1[i + 1][0] = x2;
+            arr1[i + 1][1] = y2;
+            k++;
+        }
+    }
+    for (int i = 0; i < count; i++)
+    {
+        if(arr[i][0] != arr1[i][0] && arr[i][1] != arr1[i][1])
             cout << " " << arr[i][0] << " " << arr[i][1] << endl;
     }
-        
+    cout << "Точки после востанавления" << endl;
+    for (int i = 0; i < count; i++)
+    {
+        cout << " " << arr[i][0] << " " << arr[i][1] << endl;
+    }
     return 0;
 }
